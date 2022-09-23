ab16bd10511fe9ffccd97cc605b1f2ac84681405
Leonardo Moreno <leu-profissional@hotmail.com>
2022-09-23 16:10:18 -0300

    descobrindo maior valor da array

--- introducao-a-JavaScript/dia2/lidando com arrays.js
+++ introducao-a-JavaScript/dia2/lidando com arrays.js
@@ -1,11 +1,45 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 //for (let index = 0; index < numbers.length; index += 1){
 //    console.log(numbers[index]);
 //}
-let somaTotal = 0;
 
+//////////////////////////SOMA DA ARRAY////////////////////////
+
+// let somaTotal = 0;
+// for(let index = 0; index < numbers.length; index++){
+//     somaTotal += numbers[index];    
+// }
+// console.log(somaTotal);
+
+////////////////////////MEDIA ARITMETICA//////////////////////////
+
+// let somaTotal = 0;
+// for(let index = 0; index < numbers.length; index++){
+//     somaTotal += numbers[index];    
+// }
+// console.log(somaTotal / numbers.length);
+
+//////////////////////////////////////////////////////////
+
+// let somaTotal = 0;
+// let mediaAritmetica = 0;
+// for(let index = 0; index < numbers.length; index++){
+//     somaTotal += numbers[index];    
+// }
+// mediaAritmetica = somaTotal / numbers.length;
+// if (mediaAritmetica > 20){
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor ou igual a 20');
+// }
+
+///////////////////////////////////////////////////////////
+
+let maiorValor = 0;
 for(let index = 0; index < numbers.length; index++){
-    somaTotal += numbers[index];
-    
+    if (numbers[index] > maiorValor){
+        maiorValor = numbers[index];
+    }
 }
-console.log(somaTotal);
+console.log(maiorValor);
