ec0e134d9caaa0894a8080279df2cbf6711a83e7
Leonardo Moreno <leu-profissional@hotmail.com>
2022-09-24 14:52:37 -0300

    atualiza;ao dos exercicios

--- introducao-a-JavaScript/dia3/exercicios.js
+++ introducao-a-JavaScript/dia3/exercicios.js
@@ -38,21 +38,53 @@
 // console.log(maiorPalavra);
 // console.log(menorPalavra);
 
-/////////////////////////////////////////////////////////////
+/////////////////////////////achar numero primo////////////////////////////////
 
-let  maiorNumeroPrimo = 0;
-for (let index = 2; index < 4; index += 1){
-    let numeroPrimo = true
-    for (let divisor = 2; divisor < index; divisor += 1){
-        if(index % divisor === 0){
-            numeroPrimo = false;
-        }
-    }
-    if (numeroPrimo){
-        maiorNumeroPrimo = index;
-    }
-}
-console.log(maiorNumeroPrimo);
+// let  maiorNumeroPrimo = 0;
+// for (let index = 2; index < 50; index += 1){
+//     let numeroPrimo = true;
+//     for (let divisor = 2; divisor < index; divisor += 1){
+//         if(index % divisor === 0){
+//             numeroPrimo = false;
+//         }
+//     }
+//     if (numeroPrimo){
+//         maiorNumeroPrimo = index;
+//     }
+// }
+// console.log(maiorNumeroPrimo);
      
+////////////////////////////////////////////////////////////////
 
+// let n = 5;
+// let ast = '*';
+// let input = '';
+// for (let index = 0; index < n; index += 1){
+//     input += ast;
+//     console.log(input);
+// }
+// for (let index = 0; index < n; index += 1){
+//     console.log(input);
+// }
 
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex < midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex >= controlLeft && columnIndex <= controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
