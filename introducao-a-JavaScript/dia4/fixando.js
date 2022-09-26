af30476c707af1c6322e7a52d9eeaae1cd9add43
Leonardo Moreno <leu-profissional@hotmail.com>
2022-09-26 18:13:04 -0300

    terminando o fixamento

---
+++ introducao-a-JavaScript/dia4/fixando.js
@@ -0,0 +1,37 @@
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+
+// let player = {
+//     name: 'Marta',
+//     lastName: 'Silva',
+//     age: 34,
+//     medals: { golden: 2, silver: 3},
+// };
+
+// console.log(player.name + ' ' + player.lastName + ' ' + player.age + ' ' + 'anos' + ' ' + 'possui' + ' ' + player.medals.golden + ' ' + 'medalhas de ouro e' + ' ' + player.medals.silver + ' ' + 'medalhas de prata.');
+
+///////////////////////////////////////////////////////////
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+// };
+
+// for (let key in names){
+//     console.log('Ola' + ' ' + names[key]);
+// }
+
+/////////////////////////////////////////////////////////
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let key2 in car){
+    console.log(key2 + ':' + ' ' + car[key2]);
+}
